a4390e566828503ec1bc3563337eeacbc3688070
Gaston Rodriguez <tongachapu@gmail.com>
2017-08-15 22:06:39 -0300

    pronto

---
+++ ejercicio_2/js/image_change.js
@@ -0,0 +1,15 @@
+$("figure img").hover(
+  function(){
+      var id = $(this).attr("id");
+      id = id.substr(3,1);
+      if(id === "4"){
+          $(this).attr("src","img/participantes/participante" + id + "_ganador.jpg");
+
+      }else{
+          $(this).attr("src","img/participantes/participante" + id + "_eliminado.jpg");
+      }
+  },function(){
+      var id = $(this).attr("id");
+      id = id.substr(3,1);
+      $(this).attr("src","img/participantes/participante" + id + "_simple.jpg");
+});
